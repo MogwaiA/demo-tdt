329e74075ee5d1486d1b44481780a7e52824abf6
Merwan00 <135138625+Merwan00@users.noreply.github.com>
2023-08-31 11:42:40 +0200

    Update tab_rapportseismes.py

--- tab_rapportseismes.py
+++ tab_rapportseismes.py
@@ -5,35 +5,30 @@ def rapports_seismes():
     # Personnalisation de la mise en page avec du code HTML
     st.markdown("<h1 style='text-align: left;'>Rapports en temps réel</h1>", unsafe_allow_html=True)
 
-    col_1, col_2 = st.columns(2)
+=
+    # Widget pour choisir la période
+    st.markdown("<h3>Choisir une période :</h3>", unsafe_allow_html=True)
+    period = st.selectbox(
+        "Sélectionnez la période",
+        ["Un jour", "Trois jours", "Une semaine", "Un mois", "6 mois", "Un an"]
+    )
 
-    with col_1:
-       
+    # Afficher le message d'avertissement
+    if period in ["Un mois", "6 mois", "Un an"]:
+        st.warning("Attention : plus la période choisie est longue, plus le temps d'exécution sera élevé.")
 
-    with col_2:
-        # Widget pour choisir la période
-        st.markdown("<h3>Choisir une période :</h3>", unsafe_allow_html=True)
-        period = st.selectbox(
-            "Sélectionnez la période",
-            ["Un jour", "Trois jours", "Une semaine", "Un mois", "6 mois", "Un an"]
-        )
+    # Convertir la période en nombre de jours
+    period_days = {
+        "Un jour": 1,
+        "Trois jours": 3,
+        "Une semaine": 7,
+        "Un mois": 30,
+        "6 mois": 180,
+        "Un an": 365
+    }
+    selected_days = period_days[period]
 
-        # Afficher le message d'avertissement
-        if period in ["Un mois", "6 mois", "Un an"]:
-            st.warning("Attention : plus la période choisie est longue, plus le temps d'exécution sera élevé.")
-
-        # Convertir la période en nombre de jours
-        period_days = {
-            "Un jour": 1,
-            "Trois jours": 3,
-            "Une semaine": 7,
-            "Un mois": 30,
-            "6 mois": 180,
-            "Un an": 365
-        }
-        selected_days = period_days[period]
-
-        st.write(f"Période sélectionnée : {period} ({selected_days} jours)")
+    st.write(f"Période sélectionnée : {period} ({selected_days} jours)")
     
     event_list=download_list_event(selected_days)
 
