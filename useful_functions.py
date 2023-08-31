0a0df7ad3ec58752fc24dfd86c63e60debb22c81
Merwan00 <135138625+Merwan00@users.noreply.github.com>
2023-08-31 10:17:22 +0200

    Update useful_functions.py

--- useful_functions.py
+++ useful_functions.py
@@ -12,7 +12,7 @@ from scipy.spatial.distance import cdist
 import openpyxl
 
 def load_data(file):
-    data = pd.read_csv(file) if file.name.endswith('.csv') else pd.read_excel(file, engine='openpyxl')
+    data = pd.read_csv(file,header=True,sep='') if file.name.endswith('.csv') else pd.read_excel(file,header=True, engine='openpyxl')
     return data
 
 
