7b4c48b110055ab31c7d06d2dd7087c680713519
Merwan00 <135138625+Merwan00@users.noreply.github.com>
2023-08-31 10:33:39 +0200

    Update tab_rapportseismes.py

--- tab_rapportseismes.py
+++ tab_rapportseismes.py
@@ -4,38 +4,43 @@ from useful_functions import *
 def rapports_seismes():
     # Personnalisation de la mise en page avec du code HTML
     st.markdown("<h1 style='text-align: left;'>Rapports en temps réel</h1>", unsafe_allow_html=True)
-    st.markdown("<h2 style='text-align: left;'>Visualisation des sites impactés</h2>", unsafe_allow_html=True)
 
-    # Ajouter un widget de chargement de fichier
-    uploaded_file = st.file_uploader("Charger une liste de coordonnées géographiques", type=["csv", "xlsx"])
+    col_1, col_2 = st.columns(2)
 
-    if uploaded_file is not None:
-        # Charger les données à partir du fichier
-        df = load_data(uploaded_file)
+    with col_1:
+        st.markdown("<h3 style='text-align: left;'>Sites à observer</h3>", unsafe_allow_html=True)
 
-           # Widget pour choisir la période
-    st.markdown("<h3>Choisir une période :</h3>", unsafe_allow_html=True)
-    period = st.selectbox(
-        "Sélectionnez la période",
-        ["Un jour", "Trois jours", "Une semaine", "Un mois", "6 mois", "Un an"]
-    )
+        # Ajouter un widget de chargement de fichier
+        uploaded_file = st.file_uploader("Charger une liste de coordonnées géographiques", type=["csv", "xlsx"])
 
-    # Afficher le message d'avertissement
-    if period in ["Un mois", "6 mois", "Un an"]:
-        st.warning("Attention : plus la période choisie est longue, plus le temps d'exécution sera élevé.")
+        if uploaded_file is not None:
+            # Charger les données à partir du fichier
+            df = load_data(uploaded_file)
 
-    # Convertir la période en nombre de jours
-    period_days = {
-        "Un jour": 1,
-        "Trois jours": 3,
-        "Une semaine": 7,
-        "Un mois": 30,
-        "6 mois": 180,
-        "Un an": 365
-    }
-    selected_days = period_days[period]
+    with col_2:
+        # Widget pour choisir la période
+        st.markdown("<h3>Choisir une période :</h3>", unsafe_allow_html=True)
+        period = st.selectbox(
+            "Sélectionnez la période",
+            ["Un jour", "Trois jours", "Une semaine", "Un mois", "6 mois", "Un an"]
+        )
 
-    st.write(f"Période sélectionnée : {period} ({selected_days} jours)")
+        # Afficher le message d'avertissement
+        if period in ["Un mois", "6 mois", "Un an"]:
+            st.warning("Attention : plus la période choisie est longue, plus le temps d'exécution sera élevé.")
+
+        # Convertir la période en nombre de jours
+        period_days = {
+            "Un jour": 1,
+            "Trois jours": 3,
+            "Une semaine": 7,
+            "Un mois": 30,
+            "6 mois": 180,
+            "Un an": 365
+        }
+        selected_days = period_days[period]
+
+        st.write(f"Période sélectionnée : {period} ({selected_days} jours)")
 
   
 
