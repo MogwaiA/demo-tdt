233b676b182bf3dcc33af1e36641e56b01206d7f
Merwan00 <135138625+Merwan00@users.noreply.github.com>
2023-08-31 09:44:07 +0200

    hio

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import json
 import matplotlib
 from scipy.spatial.distance import cdist
 from useful_functions import *
-from tab_eventid import *
+from tab_eventid import carte_par_eventid
 
 
 st.set_page_config(
@@ -20,7 +20,9 @@ st.set_page_config(
     initial_sidebar_state="expanded",  # Barre latérale ouverte par défaut
 )
 
-tab_eventid.carte_by_eventid
+tabs = {
+      "Visualisation Sismicité": carte_par_eventid
+}
 
 
 
