997d5e7f354ea979786a868f14ff1cde0bea07b2
Merwan00 <135138625+Merwan00@users.noreply.github.com>
2023-09-01 10:56:41 +0200

    Update tab_rapportseismes.py

--- tab_rapportseismes.py
+++ tab_rapportseismes.py
@@ -182,13 +182,15 @@ def rapports_seismes():
         if n_sites_touches==0:
             folium_static(world_map)
         else:
-            col_1, col_2 = st.columns(2)
-            with col_1:
-                folium_static(world_map, width="50%", height="100%")
-            with col_2:
-                st.markdown(f"<h4 style='text-align: left;'>Tremblement de terre ayant touché {n_sites_touches} sites pour une valeur assurée totale de {var} k€ </h1>", unsafe_allow_html=True)
-
-        st.markdown(f"<h4 style='text-align: left;'>Tremblement de terre ayant touché {n_sites_touches} sites pour une valeur assurée totale de {var} k€ </h1>", unsafe_allow_html=True)
+            st.write('<div style="display: flex; flex-direction: row;">', unsafe_allow_html=True)
+            st.write('<div style="flex: 1;">', unsafe_allow_html=True)
+            folium_static(world_map)
+            st.write('</div>', unsafe_allow_html=True)
+            st.write('<div style="flex: 1;">', unsafe_allow_html=True)
+            st.markdown(f"<h4 style='text-align: left;'>Tremblement de terre ayant touché {n_sites_touches} sites pour une valeur assurée totale de {var} k€ </h4>", unsafe_allow_html=True)
+            st.write('</div>', unsafe_allow_html=True)
+            st.write('</div>', unsafe_allow_html=True)
+            st.markdown(f"<h4 style='text-align: left;'>Tremblement de terre ayant touché {n_sites_touches} sites pour une valeur assurée totale de {var} k€ </h1>", unsafe_allow_html=True)
         
         # Créer un tableau HTML personnalisé transposé
         st.subheader("Repartition Values by Mercalli Intensity zone")
